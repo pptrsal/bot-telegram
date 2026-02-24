5a0bb1a033f12733e7afdc27fe640d261c6d19bd
pptrsal <pptrsal@gmail.com>
2026-02-24 16:22:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "bot-simrs-khanza",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "node-telegram-bot-api": "^0.64.0",
+    "mysql2": "^3.9.1",
+    "chalk": "^4.1.2",
+    "node-cron": "^3.0.3"
+  }
+}
